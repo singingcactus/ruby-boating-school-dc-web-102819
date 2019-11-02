b789955a2949ae16d1e980099fd5069242c1b460
Matt Bechtel <bechma02@gmail.com>
2019-11-02 19:28:51 +0000

    Done.

--- app/models/instructor.rb
+++ app/models/instructor.rb
@@ -9,22 +9,22 @@ class Instructor
     @@all << self
   end
 
-  def matched_test(student, test_name)
+  def match_test(student, test_name)
     BoatingTest.all.find { |test|
       test.student == student && test.test_name}
   end
 
   def pass_student(student, test_name)
-    if matched_test(student, test_name)
-      matched_test.test_status = "passed"
+    if match_test(student, test_name)
+      match_test.test_status = "passed"
     else
       BoatingTest.new(student, test_name, "passed", self)
     end
   end
 
   def fail_student(student, test_name)
-    if matched_test(student, test_name)
-      matched_test.test_status = "failed"
+    if match_test(student, test_name)
+      match_test.test_status = "failed"
     else
       BoatingTest.new(student, test_name, "failed", self)
     end
